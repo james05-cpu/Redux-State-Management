2aea563202014f1eb9160e3c7f9c42b92850ee7c
james05-cpu <muthiani.mutua1@students.jkuat.ac.ke>
2024-06-23 19:30:17 +0300

    solved bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Counter from '../src/container/appContainer';
+import Counter from './container/appContainer';
 
 class App extends Component {
    render() {
       return (
          <div className = "App">
-            <header className = "App-header">
+            
                <Counter/>
-            </header>
+            
          </div>
       );
    }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import reducer from '../src/reducer/index'
+const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+        <App />
+     </Provider>
+
 )
